2a8bf500463cbc960ab54db1972e24600226dc8e
n2waf <37115164+n2waf@users.noreply.github.com>
2019-03-15 21:20:26 +0300

    get the data from JSON file .

---
+++ AP-PLUS/ViewController/Settings.swift
@@ -0,0 +1,19 @@
+//
+//  Settings.swift
+//  AP-PLUS
+//
+//  Created by  nf on 15/03/2019.
+//
+
+import UIKit
+
+class Settings: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
